1d5a461bdcc610c6db076a2bc5fb256262f35c10
Arnav0400 <arnav.18je0156@am.iitism.ac.in>
2023-11-02 16:58:22 +0000

    added wiki perp

--- eval_gen.py
+++ eval_gen.py
@@ -1,71 +1,79 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, T5ForConditionalGeneration
+from sklearn.metrics import accuracy_score
 import torch
 from tqdm import tqdm
 from datasets import load_dataset
 from trainer import LocalTrainer
-from layers import DecomposeLinearEigen
-device = "cuda"
-model = AutoModelForCausalLM.from_pretrained(
-    "EleutherAI/gpt-neo-125m",
-    # quantization_config=bnb_config,
-    device_map={"": 0},
-    trust_remote_code=True,
-    use_auth_token=True,
-)
+from layers import DecomposeLinearEigen, DecomposeLinearEigenPrune, DecomposeLinearSVDPrune, ChannelPrune, DecomposeLinearSVD
+import argparse
+import os
+from scipy import stats
 
+parser = argparse.ArgumentParser("main")
+parser.add_argument("--layers", type=str, default='Attention.q,Attention.k,Attention.v,Attention.o,DenseReluDense.wi,DenseReluDense.wo')
+parser.add_argument("--budget", default='auto:0.95')
+parser.add_argument("--load_name", type=str, default=None)
+parser.add_argument("--baseline", type=bool, default=False)
+parser.add_argument("--algo", type=str, default='prune-eigen')
+parser.add_argument("--regress_weights", type=float, default=0.1)
+parser.add_argument("--sparsity", type=float, default=0.001)
+parser.add_argument("--dataset", type=str, default='mnli')
+args = parser.parse_args()
+
+device = "cuda"
+model = T5ForConditionalGeneration.from_pretrained("t5-small", device_map={"": 0})
 tokenizer = AutoTokenizer.from_pretrained(
-    "EleutherAI/gpt-neo-125m", 
+    "t5-small", 
     trust_remote_code=True, 
     torch_dtype="auto",
 )
 tokenizer.pad_token = tokenizer.eos_token
-dataset = load_dataset("timdettmers/openassistant-guanaco", split="train")
+dataset = load_dataset("wikitext", "wikitext-2-raw-v1", split="test")
+if not args.baseline:
+    if args.load_name is None:
+        args.load_name_folder = f'models/{args.dataset}_{args.budget}_{args.layers}_{args.algo}_regress-weights={args.regress_weights}_sparsity={args.sparsity}/'
+        paths = os.listdir(args.load_name_folder)
+        idx = 0
+        max_ckpt = 0
+        for i, path in enumerate(paths):
+            if max_ckpt<int(path.split('-')[-1]):
+                max_ckpt = int(path.split('-')[-1])
+                idx = i
+        args.load_name = f'models/{args.dataset}_{args.budget}_{args.layers}_{args.algo}_regress-weights={args.regress_weights}_sparsity={args.sparsity}/{paths[idx]}/pytorch_model.bin'
 
-trainer = LocalTrainer(
-    model=model,
-    max_seq_length=2048,
-    tokenizer=tokenizer,
-    train_dataset=dataset,
-    dataset_text_field="text",
-)
-checkpoint = torch.load('/home/ec2-user/llm_rank/data_aware/guanaco_50_all_eigen/checkpoint-6153/pytorch_model.bin')
+    trainer = LocalTrainer(
+        model=model,
+        max_seq_length=2048,
+        tokenizer=tokenizer,
+        train_dataset=dataset,
+        dataset_text_field="text",
+        layers=args.layers,
+        kappa_factor=args.budget,
+        algo=args.algo
+    )
+    checkpoint = torch.load(args.load_name)
 
-skip_idxs = []
-for i in range(12):
-    j = -1
-    for layer in ['k_proj', 'v_proj', 'q_proj', 'out_proj']:
-        j+=1
-        if f'transformer.h.{i}.attn.attention.{layer}.weight1' in checkpoint:
-            continue
-        else:
-            skip_idxs.append(i*6+j)
-    for layer in ['c_fc', 'c_proj']:
-        j+=1
-        if f'transformer.h.{i}.mlp.{layer}.weight1' in checkpoint:
-            continue
-        else:
-            skip_idxs.append(i*6+j)
+    skip_idxs = []
 
-if not f'lm_head.weight1' in checkpoint:
-    skip_idxs.append(72) 
+    trainer.decomposer_init()
+    for idx in range(len(trainer.decomposable_layers)):
+        if not idx in skip_idxs:
+            trainer.decompose_layer(index=idx)
 
-trainer.decomposer_init()
-for idx in range(len(trainer.decomposable_layers)):
-    if not idx in skip_idxs:
-        trainer.decompose_layer(index=idx)
+    for name, l in trainer.model.named_modules():
+        if isinstance(l, DecomposeLinearEigenPrune) or isinstance(l, DecomposeLinearSVDPrune) or isinstance(l, ChannelPrune) or isinstance(l, DecomposeLinearEigen) or isinstance(l, DecomposeLinearSVD):
+            if hasattr(l, 'init'):
+                l.init = True
+            if hasattr(l, 'pruned'):
+                l.pruned = True
 
-for name, layer in trainer.model.named_modules():
-    if isinstance(layer, DecomposeLinearEigen):
-        if hasattr(layer, 'init'):
-            layer.init = True
+    trainer.model.load_state_dict(checkpoint)
+    model = trainer.model
 
-trainer.model.load_state_dict(checkpoint, strict = False)
-model = trainer.model
-test = load_dataset("wikitext", "wikitext-2-raw-v1", split="test")
-encodings = tokenizer("\n\n".join(test["text"]), return_tensors="pt")
+encodings = tokenizer("\n\n".join(dataset["text"]), return_tensors="pt")
 
-max_length = model.config.max_position_embeddings
-stride = 2048
+max_length = 512#model.config.max_position_embeddings
+stride = 512
 seq_len = encodings.input_ids.size(1)
 
 nlls = []
